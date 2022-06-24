7852f81cead64729cf33658e2a7cb4f283ec824a
jvos <john.voskuyl@gmail.com>
2022-06-24 14:23:53 -0700

    finished frontend again

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
-
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
