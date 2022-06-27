a63e6cedd9722d0616e5a5fbcc99bee7b5474003
jvos <john.voskuyl@gmail.com>
2022-06-27 07:56:29 -0700

    favicon and logo created

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Footer.css';
 import { ReactComponent as LinkedIn } from "../../images/linkedin.svg"
 import { ReactComponent as GitHub } from "../../images/github.svg"
