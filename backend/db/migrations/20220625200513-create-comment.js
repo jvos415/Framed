588020f812555b8702b2d9a6478492c338705886
jvos <john.voskuyl@gmail.com>
2022-06-28 17:39:54 -0700

    removed unique key from imageId

--- backend/db/migrations/20220625200513-create-comment.js
+++ backend/db/migrations/20220625200513-create-comment.js
@@ -18,7 +18,6 @@ module.exports = {
       imageId: {
         type: Sequelize.INTEGER,
         allowNull: false,
-        unique: true,
         references: {
           model: "Images"
         }
