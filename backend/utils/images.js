baa7b0fd2787d4a2fd1ccd0646e41fe9f8f3c6c9
jvos <john.voskuyl@gmail.com>
2022-06-29 13:27:24 -0700

    image validations done

--- backend/utils/images.js
+++ backend/utils/images.js
@@ -1,30 +1,35 @@
-const { check } = require('express-validator');
-const { handleValidationErrors } = require('./validation');
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("./validation");
 
-const imageUrl = check('imageUrl')
+const imageUrl = check("imageUrl")
   .notEmpty()
-  .withMessage('Image Url cannot be empty')
+  .withMessage("Image Url cannot be empty")
   .isLength({ min: 4 })
-  .withMessage('Image Url cannot be less than 4 characters')
+  .withMessage("Image Url cannot be less than 4 characters")
   .isLength({ max: 250 })
-  .withMessage('Image Url cannot be greater than 250 characters')
-  // .isURL({ require_protocol: false, require_host: false });
-const title = check('title')
+  .withMessage("Image Url cannot be greater than 250 characters")
+// .isURL({ require_protocol: false, require_host: false });
+const title = check("title")
   .notEmpty()
-  .withMessage('Image title cannot be empty')
+  .withMessage("Image title cannot be empty")
   .isLength({ min: 1 })
-  .withMessage('Image title cannot be less than 4 characters')
+  .withMessage("Image title cannot be less than 4 characters")
   .isLength({ max: 40 })
-  .withMessage('Image title cannot be greater than 40 characters')
+  .withMessage("Image title cannot be greater than 40 characters")
+const description = check("description")
+  .isLength({ max: 150 })
+  .withMessage("Description cannot be more than 150 characters long")
 
 exports.validateAddPhoto = [
   imageUrl,
   title,
+  description,
   handleValidationErrors
 ];
 
 exports.validateUpdatePhoto = [
   imageUrl,
   title,
+  description,
   handleValidationErrors
 ];
