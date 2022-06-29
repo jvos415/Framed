d74972c5d96a2e4d3da13b55e89ff3d6e115087a
jvos <john.voskuyl@gmail.com>
2022-06-28 17:32:47 -0700

    comment seeder done

---
+++ backend/db/seeders/20220629001936-commentSeeder.js
@@ -0,0 +1,89 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+   return queryInterface.bulkInsert('Comments', [
+    {
+     userId: 1,
+     imageId: 3,
+     comment: "This photo is the bestest photo evvvvaaa!",
+     createdAt: new Date(),
+     updatedAt: new Date()
+   },
+   {
+    userId: 1,
+    imageId: 5,
+    comment: "This photo is not my favorite ;)",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    userId: 3,
+    imageId: 12,
+    comment: "Leaving a comment because I can",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    userId: 2,
+    imageId: 9,
+    comment: "This rules! Keep up the good work!",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    userId: 2,
+    imageId: 9,
+    comment: "This rules! Keep up the good work!",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    userId: 4,
+    imageId: 9,
+    comment: "Great stuff!",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    userId: 1,
+    imageId: 7,
+    comment: "Love it!",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    userId: 2,
+    imageId: 7,
+    comment: "Love it more!",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    userId: 2,
+    imageId: 16,
+    comment: "This is incredible! Nice shot!",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    userId: 2,
+    imageId: 17,
+    comment: "Woooohhhhooooooo!",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    userId: 5,
+    imageId: 10,
+    comment: "This is a comment, a real live comment",
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+   return queryInterface.bulkDelete('Comments', null, {});
+  }
+};
