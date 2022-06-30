7c8427236790f9e4b417605c1151f139f6776637
jvos <john.voskuyl@gmail.com>
2022-06-29 18:12:38 -0700

    fixed crash when not logged in and clicking on a photo

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -1,41 +1,62 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
-import './SplashPage.css'
+import "./SplashPage.css";
 
-import { getImages } from '../../store/images';
+import { getImages } from "../../store/images";
 
 const ImageScroll = () => {
+  const sessionUser = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
   const allImages = useSelector((state) => {
     return Object.values(state.images);
   });
 
-  const dispatch = useDispatch()
-
-  useEffect(()=> {
-    dispatch(getImages())
-  }, [dispatch])
+  useEffect(() => {
+    dispatch(getImages());
+  }, [dispatch]);
 
   if (!allImages) {
     return null;
   }
 
-  return (
-    <div className='splash-container'>
-      {allImages.map((image) => {
-        return (
-          <Link id="splash-images" key={image.id} to={`/images/${image.id}`}>
-            <img key={image.id} id="splash-images"
+  let content = null;
+
+  if (sessionUser) {
+    content = allImages.map((image) => {
+      return (
+        <Link id="splash-images" key={image.id} to={`/images/${image.id}`}>
+          <img
+            key={image.id}
+            id="splash-images"
             src={`${image.imageUrl}`}
             alt={`${image.title}`}
-            loading="lazy"></img>
-          </Link>
-        )
-      })}
+            loading="lazy"
+          ></img>
+        </Link>
+      );
+    });
+  } else {
+    content = allImages.map((image) => {
+      return (
+          <img
+            key={image.id}
+            id="splash-images"
+            src={`${image.imageUrl}`}
+            alt={`${image.title}`}
+            loading="lazy"
+          ></img>
+      );
+    });
+  }
+
+  return (
+    <div className="splash-container">
+      {content}
       <button onClick={() => window.scrollTo(0, 0)}>Back to the top</button>
     </div>
-  )
-}
+  );
+};
 
 export default ImageScroll;
