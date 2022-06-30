ceb1070abe4850e46fa4771d02f0204e800db13b
jvos <john.voskuyl@gmail.com>
2022-06-29 17:27:41 -0700

    little styling fixes

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -8,14 +8,14 @@ function Footer(){
   return (
     <ul className="footer">
       <li className="footer-items">
-      <p>Developer: John Voskuyl</p>
+      <p>Created By John Voskuyl</p>
       <a href="https://www.linkedin.com/in/john-voskuyl-a2214083">
         <LinkedIn id="linkedIn" />
       </a>
       <a href="https://github.com/jvos415">
         <GitHub id="GitHub"/>
       </a>
-       <p id="established"><span>&copy;</span>FRAMED 2022</p>
+       <p id="established"><span>&copy;</span> FRAMED 2022</p>
       </li>
     </ul>
   );
