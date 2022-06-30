a709c52967ba7f962e54239522b5b0991c40d5c8
jvos <john.voskuyl@gmail.com>
2022-06-29 18:56:08 -0700

    custom 404 page

---
+++ frontend/src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,13 @@
+import "./PageNotFound.css"
+
+const PageNotFound = () => {
+
+  return (
+    <div className="page-not-found">
+      <h3 id="page-not-found-title">Page Not Found!</h3>
+      <p id="page-not-found-body">The Page you are looking for cannot be found</p>
+    </div>
+  )
+}
+
+export default PageNotFound;
