9cefb3d177583eefcaafd4625339656c00ffe2ef
jvos <john.voskuyl@gmail.com>
2022-07-01 08:37:58 -0700

    fixed anchor tags

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -9,10 +9,10 @@ function Footer(){
     <ul className="footer">
       <li className="footer-items">
       <p>Created By John Voskuyl</p>
-      <a href="https://www.linkedin.com/in/john-voskuyl-a2214083">
+      <a href="https://www.linkedin.com/in/john-voskuyl-a2214083" target="blank">
         <LinkedIn id="linkedIn" />
       </a>
-      <a href="https://github.com/jvos415">
+      <a href="https://github.com/jvos415" target="blank">
         <GitHub id="GitHub"/>
       </a>
        <p id="stack-items">Built using React, Redux, PostgreSQL, Express.js, Node.js</p>
