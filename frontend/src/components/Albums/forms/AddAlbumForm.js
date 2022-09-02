4c7efb874882991669c167fb0283843635fc0b19
Jvos <john.voskuyl@gmail.com>
2022-09-01 17:04:26 -0700

    working on add albums page

--- frontend/src/components/Albums/forms/AddAlbumForm.js
+++ frontend/src/components/Albums/forms/AddAlbumForm.js
@@ -1,10 +1,44 @@
-import React from "react"
+import React, { useState } from "react"
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { createAlbum } from "../../../store/albums";
 
 const AddAlbumForm = () => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const user = useSelector((state) => state.session.user);
+
+    const [title, setTitle] = useState("");
+    const [errors, setErrors] = useState([]);
+
+    const submitAlbum = async (e) => {
+        e.preventDefault();
+
+        const userId = user.id;
+
+        const albumObj = {
+            userId,
+            title
+        }
+
+        try {
+            let createdAlbum = await dispatch(createAlbum(albumObj));
+      
+            if (createdAlbum) {
+              return history.push(`/my-albums/${userId}`);
+            }
+          } catch (error) {
+            const data = await error.json();
+              if (data && data.errors) setErrors(data.errors);
+          }
+    }
 
     return (
         <div>
-            <h1>This will be the add album page</h1>
+            <h1>Add an Photo Album</h1>
+            <form onSubmit={submitAlbum}>
+
+            </form>
         </div>
     )
 }
