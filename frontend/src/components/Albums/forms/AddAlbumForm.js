b09599b399b3997d91d898213e5d10a7b583f653
Jvos <john.voskuyl@gmail.com>
2022-09-01 17:12:53 -0700

    create albums working

--- frontend/src/components/Albums/forms/AddAlbumForm.js
+++ frontend/src/components/Albums/forms/AddAlbumForm.js
@@ -1,46 +1,82 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { createAlbum } from "../../../store/albums";
 
 const AddAlbumForm = () => {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const user = useSelector((state) => state.session.user);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const user = useSelector((state) => state.session.user);
 
-    const [title, setTitle] = useState("");
-    const [errors, setErrors] = useState([]);
+  const [title, setTitle] = useState("");
+  const [errors, setErrors] = useState([]);
 
-    const submitAlbum = async (e) => {
-        e.preventDefault();
+  const submitAlbum = async (e) => {
+    e.preventDefault();
 
-        const userId = user.id;
+    const userId = user.id;
 
-        const albumObj = {
-            userId,
-            title
-        }
+    const albumObj = {
+      userId,
+      title,
+    };
 
-        try {
-            let createdAlbum = await dispatch(createAlbum(albumObj));
-      
-            if (createdAlbum) {
-              return history.push(`/my-albums/${userId}`);
-            }
-          } catch (error) {
-            const data = await error.json();
-              if (data && data.errors) setErrors(data.errors);
-          }
+    try {
+      let createdAlbum = await dispatch(createAlbum(albumObj));
+
+      if (createdAlbum) {
+        return history.push(`/my-albums/${userId}`);
+      }
+    } catch (error) {
+      const data = await error.json();
+      if (data && data.errors) setErrors(data.errors);
     }
+  };
 
-    return (
-        <div>
-            <h1>Add an Photo Album</h1>
-            <form onSubmit={submitAlbum}>
+  const handleCancelClick = (e) => {
+    e.preventDefault();
 
-            </form>
-        </div>
-    )
-}
+    return history.push("/");
+  };
+
+  let errorList;
+
+  if (errors.length > 0) {
+    errorList = "errorList";
+  } else {
+    errorList = "";
+  }
+
+  let footer = document.querySelector(".footer");
+  if (footer) {
+    footer.classList.add("footer-position");
+  }
+
+  return (
+    <div>
+      <h1>Add an Photo Album</h1>
+      <form onSubmit={submitAlbum}>
+        <ul className={errorList}>
+          {errors.map((error, idx) => (
+            <li key={idx}>{error}</li>
+          ))}
+        </ul>
+        <div className="form-box">
+          <label className="label">Album Title</label>
+          <input className="input-field"
+            type="text"
+            placeholder="Album Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <button id="button-add" type="submit">Create Album</button>
+          <button  id="button-cancel-add" type="button" onClick={handleCancelClick}>
+            Cancel
+          </button>
+          </div>
+      </form>
+    </div>
+  );
+};
 
 export default AddAlbumForm;
