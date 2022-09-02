885d9ba841522d8b3f011dad289a4965b351cf61
Jvos <john.voskuyl@gmail.com>
2022-09-01 20:28:12 -0700

    working on photo albums page

--- frontend/src/components/Albums/MyAlbumsPage.js
+++ frontend/src/components/Albums/MyAlbumsPage.js
@@ -1,12 +1,29 @@
-import React from "react"
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./myAlbumsPage.css"
 
 const MyAlbumsPage = () => {
+  const history = useHistory();
 
-    return (
-        <div>
-            <h1>This page will display all the user's albums</h1>
-        </div>
-    )
-}
+  const newAlbumPageFunc = () => {
+    history.push("/add-album");
+  };
+
+  let footer = document.querySelector(".footer");
+  if (footer) {
+    footer.classList.add("footer-position");
+  }
+
+  return (
+    <div className="my-albums-container">
+      <h2>My Photo Albums</h2>
+      <div className="add-new-album">
+        <button id="button-cancel-add" type="button" onClick={newAlbumPageFunc}>
+          New Photo Album
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default MyAlbumsPage;
--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -22,7 +22,7 @@ function Navigation({ isLoaded }){
     sessionLinks = (
       <>
         <NavLink id="add-image" exact to="/add-image">Add Image</NavLink>
-        {/* the link above needs to go to an add-photo page, maybe make this a cool icon*/}
+        <NavLink id="add-image" exact to={`/my-albums/${sessionUser.id}`}>My Albums</NavLink>
         <ProfileButton user={sessionUser} />
       </>
     );
@@ -30,12 +30,11 @@ function Navigation({ isLoaded }){
     sessionLinks = (
       <div className="logged-in-nav">
         <button id="demo-user-button" onClick = {demoUserLogin}>Demo User</button>
-        {/* the link needs to log you in as a demo user */}
         <NavLink id="login-button"  to="/login">Log In</NavLink>
         <NavLink id="signup-button" to="/signup">Sign Up</NavLink>
       </div>
     );
-  }
+  };
 
   return (
     <div className="nav-bar">
