e45297c4432b6c6d73ca431ff278009ce588aa76
Jvos <john.voskuyl@gmail.com>
2022-09-02 22:13:27 -0700

    minor change in use profile menu

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import { useHistory } from "react-router-dom";
-
 import * as sessionActions from '../../store/session';
 import './Navigation.css'
 
