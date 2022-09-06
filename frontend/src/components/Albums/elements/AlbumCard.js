0144105b340453ca6c256f0c6c2472e17e6f4d81
Jvos <john.voskuyl@gmail.com>
2022-09-06 08:38:00 -0700

    working on last bug for deleting an album with images in it

--- frontend/src/components/Albums/elements/AlbumCard.js
+++ frontend/src/components/Albums/elements/AlbumCard.js
@@ -9,16 +9,20 @@ import "./albumCard.css";
 const AlbumCard = ({ album }) => {
   const dispatch = useDispatch();
   const history = useHistory();
-  const images = Object.values(useSelector((state) => state.images)).filter((image) => {
-    return image.albumId === album.id
-  });
-  const user = useSelector((state) => state.session.user)
+  const images = Object.values(useSelector((state) => state.images)).filter(
+    (image) => {
+      return image.albumId === album.id;
+    }
+  );
+  const user = useSelector((state) => state.session.user);
+
+  const [image, setImage] = useState(null);
 
   useEffect(() => {
     if (!user) {
       return history.push("/");
     }
-  },[user, history])
+  }, [user, history]);
 
   useEffect(() => {
     dispatch(getImages());
@@ -27,25 +31,25 @@ const AlbumCard = ({ album }) => {
   const [updateAlbumComp, setUpdateAlbumComp] = useState("");
 
   const updateAlbumComponent = () => {
-    setUpdateAlbumComp(true)
+    setUpdateAlbumComp(true);
   };
 
   const allAlbumIdsToNull = () => {
-    images.forEach(async (image) => {
-      const imageId = image.id
-      const userId = image.userId;
+    images.forEach(async (imageObj) => {
+      const imageId = imageObj.id;
+      const userId = imageObj.userId;
       const albumId = null;
-      const imageUrl = image.imageUrl;
-      const title = image.title;
-      const description = image.description;
-      const createdAt = image.createdAt;
+      // const imageUrl = image.imageUrl;
+      const title = imageObj.title;
+      const description = imageObj.description;
+      const createdAt = imageObj.createdAt;
       const updatedAt = new Date();
 
       const payload = {
         id: imageId,
         albumId,
         userId,
-        imageUrl,
+        image,
         title,
         description,
         createdAt,
@@ -57,34 +61,66 @@ const AlbumCard = ({ album }) => {
       if (updatedImage) {
         return history.push(`/my-albums/${user.id}`);
       }
-    })
+    });
+  };
+
+  const updateFile = (e) => {
+    const file = e.target.files[0];
+    if (file) setImage(file);
   };
 
   const deleteAlbumFunc = () => {
     allAlbumIdsToNull();
     dispatch(getImages());
-    dispatch(deleteAlbum(album.id))
+    dispatch(deleteAlbum(album.id));
     // This delete needs to be in here twice for it to work!
-    dispatch(deleteAlbum(album.id))
+    dispatch(deleteAlbum(album.id));
   };
 
   return (
     <div className="album-card-container">
       <div className="link-container">
-        <Link style={{ textDecoration: 'none' }} exact="true" to={`/albums/${album.id}`}>
+        <Link
+          style={{ textDecoration: "none" }}
+          exact="true"
+          to={`/albums/${album.id}`}
+        >
           <h2 id="album-title">{album.title}</h2>
         </Link>
       </div>
       <div>
-        {!updateAlbumComp && (<button id="button-cancel-add" type="button" onClick={updateAlbumComponent}>
-          Update Album Title
-        </button>)}
-        {!updateAlbumComp && (<button id="button-cancel-add" type="button" onClick={deleteAlbumFunc}>
-          Delete Album
-        </button>)}
-        {updateAlbumComp && (
-            <EditAlbumForm setUpdateAlbumComp={setUpdateAlbumComp} album={album}/>
+        {!updateAlbumComp && (
+          <button
+            id="button-cancel-add"
+            type="button"
+            onClick={updateAlbumComponent}
+          >
+            Update Album Title
+          </button>
         )}
+        {!updateAlbumComp && (
+          <button
+            id="button-cancel-add"
+            type="button"
+            onClick={deleteAlbumFunc}
+          >
+            Delete Album
+          </button>
+        )}
+        {updateAlbumComp && (
+          <EditAlbumForm
+            setUpdateAlbumComp={setUpdateAlbumComp}
+            album={album}
+          />
+        )}
+        <label id="hide-me">
+          <input
+            id="upload-image"
+            className="input-field"
+            type="file"
+            onChange={updateFile}
+          />
+        </label>
       </div>
     </div>
   );
